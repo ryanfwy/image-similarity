322693ae55d12d863f2f8798e58e35f53e1b00ad
Ryan <369694496@qq.com>
2019-03-05 21:12:04 +0800

    Fix: add `User-Agent` while requesting

--- main_multi.py
+++ main_multi.py
@@ -101,7 +101,8 @@ class ImageSimilarity():
         '''
         path, fields = para['path'], para['fields']
         try:
-            res = requests.get(path)
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
+            res = requests.get(path, headers=headers)
             feature = DeepModel.preprocess_image(BytesIO(res.content))
             return feature, fields
 
